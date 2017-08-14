11c4bd5495063437a56767da48c0e10c594b20ba
Alex Zi <ale8270@yandex.ru>
2017-08-14 15:55:22 +0300

    Init Comment model

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170814110920) do
+ActiveRecord::Schema.define(version: 20170814125201) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "comments", force: :cascade do |t|
+    t.text "body"
+    t.bigint "user_id"
+    t.bigint "news_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["news_id"], name: "index_comments_on_news_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
+  end
 
   create_table "news", force: :cascade do |t|
     t.string "title"
@@ -42,5 +52,7 @@ ActiveRecord::Schema.define(version: 20170814110920) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "comments", "news"
+  add_foreign_key "comments", "users"
   add_foreign_key "news", "users"
 end
